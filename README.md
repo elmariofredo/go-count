2dd497ce40d4ea1493fe79ad69c1b3d70f931e0b
Mario Vejlupek <mario@vejlupek.cz>
2021-05-02 12:53:17 +0200

    Add /count endpoint

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 I'm going to build counter service with following features
 
 - [ ] HTTP endpoint response with increasing number on every call
+- [ ] Tests 👻
+- [ ] JSON logging with verbosity level
 - [ ] Reset request
 - [ ] Database backend to preserve counter during restart
 - [ ] CI
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"strconv"
+)
+
 func main() {
-	println("Ahoj")
+	println("Starting")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("Ahoj"))
+	})
+
+	counter := 1
+
+	http.HandleFunc("/count", func(rw http.ResponseWriter, r *http.Request) {
+		
+		counter++
+
+		rw.Write([]byte(strconv.Itoa(counter)))
+
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
