222121abf0703a1d4b01e9c802e8721e54ce534c
Mario Vejlupek <mario@vejlupek.cz>
2021-07-21 12:00:48 +0200

    👾 Test terraform

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+
+  backend "remote" {
+    organization = "elmariofredo"
+    workspaces {
+      name = "go-count"
+    }
+  }
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.10.1"
+    }
+  }
+}
+
+# Add token to terraform cloud
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "web" {
+  name = "test"
+  size = "s-1vcpu-2gb"
+  region = "fra1"
+  image = "ubuntu-18-04-x64"
+}
