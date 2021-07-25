d852a1444c01df8a55f8dad7ceb73d1c73c9d62d
Mario Vejlupek <mario@vejlupek.cz>
2021-07-25 22:35:04 +0200

    Install ingress

--- main.tf
+++ main.tf
@@ -23,6 +23,18 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+
+provider "helm" {
+  # kubernetes {
+  #   config_path = "~/.kube/config"
+  # }
+
+}
+
 resource "digitalocean_kubernetes_cluster" "test" {
   name = "test"
   region = "ams3"
@@ -34,4 +46,16 @@ resource "digitalocean_kubernetes_cluster" "test" {
     node_count = 1
   }
 
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
 }
